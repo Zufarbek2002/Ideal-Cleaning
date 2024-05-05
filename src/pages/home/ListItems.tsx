48563f75df5e738132ecb847fb93322748925306
Zufarbek <game123qwerty00@gmail.com>
2024-05-05 19:32:41 +0500

    init

--- src/pages/home/ListItems.tsx
+++ src/pages/home/ListItems.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -11,41 +12,53 @@ import SettingsIcon from "@mui/icons-material/Settings";
 
 export const mainListItems = (
   <React.Fragment>
-    <ListItemButton>
-      <ListItemIcon>
-        <HomeIcon />
-      </ListItemIcon>
-      <ListItemText primary="Asosiy" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Buyurtmalar" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Mijozlar" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <MailOutlineIcon />
-      </ListItemIcon>
-      <ListItemText primary="SMS marketing" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Xizmatlar" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <SettingsIcon />
-      </ListItemIcon>
-      <ListItemText primary="Sozlamalar" />
-    </ListItemButton>
+    <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+      <ListItemButton>
+        <ListItemIcon>
+          <HomeIcon />
+        </ListItemIcon>
+        <ListItemText primary="Asosiy" />
+      </ListItemButton>
+    </Link>
+    <Link to="/orders" style={{ color: "inherit", textDecoration: "none" }}>
+      <ListItemButton>
+        <ListItemIcon>
+          <ShoppingCartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Buyurtmalar" />
+      </ListItemButton>
+    </Link>
+    <Link to="/customers" style={{ color: "inherit", textDecoration: "none" }}>
+      <ListItemButton>
+        <ListItemIcon>
+          <PeopleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Mijozlar" />
+      </ListItemButton>
+    </Link>
+    <Link to="/smm" style={{ color: "inherit", textDecoration: "none" }}>
+      <ListItemButton>
+        <ListItemIcon>
+          <MailOutlineIcon />
+        </ListItemIcon>
+        <ListItemText primary="SMS marketing" />
+      </ListItemButton>
+    </Link>
+    <Link to="/services" style={{ color: "inherit", textDecoration: "none" }}>
+      <ListItemButton>
+        <ListItemIcon>
+          <LayersIcon />
+        </ListItemIcon>
+        <ListItemText primary="Xizmatlar" />
+      </ListItemButton>
+    </Link>
+    <Link to="/settings" style={{ color: "inherit", textDecoration: "none" }}>
+      <ListItemButton>
+        <ListItemIcon>
+          <SettingsIcon />
+        </ListItemIcon>
+        <ListItemText primary="Sozlamalar" />
+      </ListItemButton>
+    </Link>
   </React.Fragment>
 );
